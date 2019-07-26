9a874a19f2856ba92baf3b9b2cf6b6d2abf9110a
mcampaz3 <mcampaz@gatech.edu>
2019-07-26 02:30:38 -0700

    add validation on models

--- DojoSurveyModels/Models/DojoComment.cs
+++ DojoSurveyModels/Models/DojoComment.cs
@@ -1,9 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DojoSurveyModels.Models
 {
     public class DojoComment
     {
+        [Required]
+        [Display(Name = "Your Name:")]
         public string Name { get; set; }
         public string DojoLocation { get; set; }
         public string FavoriteLanguage { get; set; }
