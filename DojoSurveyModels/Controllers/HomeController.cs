06ec85e1d58153e6f2f7e8c47020c5bae62de0cc
mcampaz3 <mcampaz@gatech.edu>
2019-07-26 01:58:02 -0700

    Acceptable Dojo Survey with Models project

--- DojoSurveyModels/Controllers/HomeController.cs
+++ DojoSurveyModels/Controllers/HomeController.cs
@@ -7,17 +7,58 @@ using Microsoft.AspNetCore.Mvc;
 using DojoSurveyModels.Models;
 
 namespace DojoSurveyModels.Controllers
-{
+{    
+
+
     public class HomeController : Controller
     {
+        List<string> location = new List<string>(){
+            "Berkeley, CA",
+            "Boise, ID",
+            "Chicago, IL",
+            "Dallas, TX",
+            "Los Angeles, CA",
+            "Orange County, CA",
+            "Seattle, WA",
+            "Silicon Valley, CA",
+            "Tulsa, OK",
+            "Tysons Corner, VA",
+            "Online"
+        };
+        List<string> language = new List<string>(){
+            "C",
+            "C++",
+            "C#",
+            "Objective C",
+            "Java",
+            "Basic",
+            "JavaScript",
+            "Python",
+            "Scala",
+            "Go",
+            "Swift",
+            "Ruby"
+        };
         public IActionResult Index()
-        {
+        {            
+            ViewBag.language = this.language;
+            ViewBag.location = this.location;
+            Console.WriteLine(ViewBag);
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost("commentresult")]
+        public IActionResult CommentResult(string name, string location, string language, string message)
         {
-            return View();
+            DojoComment model = new DojoComment()
+            {
+                Name = name,
+                DojoLocation = location,
+                FavoriteLanguage = language,
+                Comment = message
+            };
+            
+            return View("CommentResult", model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ DojoSurveyModels/Models/DojoComment.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DojoSurveyModels.Models
+{
+    public class DojoComment
+    {
+        public string Name { get; set; }
+        public string DojoLocation { get; set; }
+        public string FavoriteLanguage { get; set; }
+        public string Comment { get; set; }
+    }
+}
