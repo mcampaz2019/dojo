e8dfb074a63d85b42a85654ee4f40e51abd86c8b
mcampaz3 <mcampaz@gatech.edu>
2019-07-28 09:02:46 -0700

    Setup a Random Password Generator  App

---
+++ RandomPasswordGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RandomPasswordGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
