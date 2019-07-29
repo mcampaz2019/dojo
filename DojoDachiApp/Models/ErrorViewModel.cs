cb2a1bebcc0c0ac86beecf6f532556dff037dbbb
Mario Campaz <mcampaz2019@gmail.com>
2019-07-29 11:36:56 -0700

    Initial setup

---
+++ DojoDachiApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DojoDachiApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
