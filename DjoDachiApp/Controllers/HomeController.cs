0a967b1ee89587b63dd666c1e5407d35bb715502
Mario Campaz <mcampaz2019@gmail.com>
2019-07-29 12:00:58 -0700

    Setup controller, game razor page

--- DjoDachiApp/Controllers/HomeController.cs
+++ DjoDachiApp/Controllers/HomeController.cs
@@ -10,13 +10,16 @@ namespace DjoDachiApp.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+
+            return RedirectToAction("dojodachi");
         }
 
-        public IActionResult Privacy()
-        {
+        [HttpGet("dojodachi")]
+        public IActionResult DojoDachi(){
+
             return View();
         }
 
@@ -26,4 +29,23 @@ namespace DjoDachiApp.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+
+    /*
+    to create a virtual pet, a Dojodachi! Dojodachis have happiness, fullness, and energy that we need to max out. 
+    You can feed, play with, work, or sleep to manage these values. if you can get your Dojodachi's fullness, 
+    happiness, and energy above 100, you win! But if fullness or happiness falls to zero, your Dojodachi will die :(
+
+    Your Dojodachi should start with 20 happiness, 20 fullness, 50 energy, and 3 meals.
+    After every button press, display a message showing how the Dojodachi reacted
+    Feeding your Dojodachi costs 1 meal and gains a random amount of fullness between 5 and 10 
+    (you cannot feed your Dojodachi if you do not have meals)
+    Playing with your Dojodachi costs 5 energy and gains a random amount of happiness between 5 and 10
+    Every time you play with or feed your dojodachi there should be a 25% chance that it won't like it. 
+    Energy or meals will still decrease, but happiness and fullness won't change.
+    Working costs 5 energy and earns between 1 and 3 meals
+    Sleeping earns 15 energy and decreases fullness and happiness each by 5
+    If energy, fullness, and happiness are all raised to over 100, you win! a restart button should be displayed.
+    If fullness or happiness ever drop to 0, you lose, and a restart button should be displayed.
+     */
+
 }
