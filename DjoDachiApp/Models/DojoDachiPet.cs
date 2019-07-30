c19fff25e9ae8ed606144bc06cf713e92b234884
Mario Campaz <mcampaz2019@gmail.com>
2019-07-29 19:11:19 -0700

    setup pet class and started to set up the view

--- DjoDachiApp/Models/DojoDachiPet.cs
+++ DjoDachiApp/Models/DojoDachiPet.cs
@@ -5,31 +5,45 @@ namespace DjoDachiApp.Models
         public int Happiness { get; set; }
         public int Fullness { get; set; }
         public int Energy { get; set; }
+        public int Meals { get; set; }
 
-        public bool IsFull => !string.IsNullOrEmpty(RequestId);
-
-        public void feed(){
-
+        public DojoDachiPet(int happiness = 20, int fullness = 20, int energy = 50, int meals = 3)
+        {
+            Happiness = happiness;
+            Fullness = fullness;
+            Energy = energy;
+            Meals = meals;
         }
 
+        public bool IsFull => (Happiness > 100 && Fullness > 100 && Energy > 100);
+
+        public bool IsDead => (Happiness <= 0 || Fullness <= 0);
+
         public void playWith(){
-            
+            if(Meals > 0)
+            {
+                Energy -= 5;
+                Happiness += selector(5, 10);
+            }
         }
 
         public void work(){
-            
+            Energy -= 5;
+            Meals += selector(1, 3);
         }
 
         public void sleep(){
-            
+            Energy += 15;
+            Fullness -=5;
+            Happiness -= 5;
         }
 
         public void feed(){
-            
+            if(Meals > 0)
+            {
+                Energy += selector(5, 10);
+                Meals--;
+            }
         }
-
-        /*
-        
-         */
     }
 }
