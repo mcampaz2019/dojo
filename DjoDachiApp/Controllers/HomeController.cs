90458478737a9ed2684d96996eb3e4d44a07bf54
Mario Campaz <mcampaz2019@gmail.com>
2019-07-31 10:53:52 -0700

    Pause, setting up button actions

--- DjoDachiApp/Controllers/HomeController.cs
+++ DjoDachiApp/Controllers/HomeController.cs
@@ -11,24 +11,35 @@ namespace DjoDachiApp.Controllers
 {
     public class HomeController : Controller
     {
-        DojoDachiPet dachiPet = new DojoDachiPet();
+        
 
         [HttpGet]
         public IActionResult Index()
         {
-            
+            DojoDachiPet dachiPet = new DojoDachiPet();
             HttpContext.Session.SetObjectAsJson("myDachiPet", dachiPet);
-            return RedirectToAction("dojodachi");
+            return RedirectToAction("dojodachi", dachiPet);
         }
 
         [HttpGet("dojodachi")]
-        public IActionResult DojoDachi(){
+        public IActionResult DojoDachi(DojoDachiPet thisDachiPet){
             DojoDachiPet thisDachiPet = HttpContext.Session.GetObjectFromJson<DojoDachiPet>("myDachiPet");
-            ViewBag.Fullness = thisDachiPet.Fullness;
-            ViewBag.Happiness = thisDachiPet.Happiness;
-            ViewBag.Meals = thisDachiPet.Meals;
-            ViewBag.Energy = thisDachiPet.Energy;
-            return View();
+            // ViewBag.Fullness = thisDachiPet.Fullness;
+            // ViewBag.Happiness = thisDachiPet.Happiness;
+            // ViewBag.Meals = thisDachiPet.Meals;
+            // ViewBag.Energy = thisDachiPet.Energy;
+            return View("dojodachi", thisDachiPet);
+        }
+
+        [HttpPost("processMove")]
+        public IActionResult ProcessMove(string move){
+            DojoDachiPet thisDachiPet = HttpContext.Session.GetObjectFromJson<DojoDachiPet>("myDachiPet");
+            if(move == "play"){thisDachiPet.playWith();}
+            if(move == "feed"){thisDachiPet.feed();}
+            if(move == "sleep"){thisDachiPet.sleep();}
+            if(move == "work"){thisDachiPet.work();}
+            HttpContext.Session.SetObjectAsJson("myDachiPet", thisDachiPet);
+            return RedirectToAction("dojodachi", thisDachiPet);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- DjoDachiApp/Models/DojoDachiPet.cs
+++ DjoDachiApp/Models/DojoDachiPet.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace DjoDachiApp.Models
 {
     public class DojoDachiPet
