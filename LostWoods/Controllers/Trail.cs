72bc3b9847fcf791e73944ee1fa713febc647840
Mario Campaz <mcampaz2019@gmail.com>
2019-08-02 18:45:55 -0700

    Initial UI setup

---
+++ LostWoods/Controllers/Trail.cs
@@ -0,0 +1,10 @@
+namespace LostWoods.Controllers
+{
+    public class Trail
+    {
+        public string Trail_Name { get; set; }
+        public double ElevationGain { get; set; }
+        public double MyProperty { get; set; }
+
+    }
+}
